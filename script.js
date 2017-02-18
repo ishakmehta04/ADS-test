989e22c1d747f0feed1fce355164e2c860f8a42d
ishakmehta04 <ishakmehta04@gmail.com>
2017-02-18 10:22:27 -0500

    getting data from json using vue before create

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 var VueComp = new Vue({
     el: ".vehicle-coverage-tables",
+    beforeCreate: function() {
+        var self = this;
+        $.getJSON("coverage.json", function(jsdata) {
+            self.car = jsdata;
+        });
+    },
     data: {
-        "vehicle_models":["ILX","MDX","RDX","RLX","TL","TLX","TSX"],
-        "years":[2018,2017,2016,2015,2014,2013,2012,2011,2010],
-        "coverage":{
-            "ILX":[2017,2016,2015,2014],
-            "MDX":[2017,2016,2015,2014],
-            "RDX":[2011,2010],
-            "RLX":[2012,2011,2010],
-            "TL":[2014,2013,2012,2011,2010],
-            "TLX":[2016,2015,2014,2013],
-            "TSX":[2017,2015]
-        }, 
+        car:'',
     },
     methods: {
         setPill : function() {
@@ -21,6 +17,6 @@ var VueComp = new Vue({
                 $(targetElement).removeClass('set')
             else 
                 $(targetElement).addClass('set')
-        }
+        },    
     }
 })
